baaa1b77dbc3a144d53e05efbff377326cc8a31a
Prasad <sawantp500@gmail.com>
2026-09-10 18:59:22 +0530

    feat(ui): modernize share_app_page

--- lib/features/settings/presentation/share_app_page.dart
+++ lib/features/settings/presentation/share_app_page.dart
@@ -4,6 +4,7 @@ import 'package:pos_billingwala_v2/core/constants/app_colors.dart';
 import 'package:pos_billingwala_v2/core/constants/app_constants.dart';
 import 'package:pos_billingwala_v2/core/widgtes/widgtes.dart';
 import 'package:pos_billingwala_v2/core/widgets/app_module_icon.dart';
+import 'package:pos_billingwala_v2/core/widgets/app_module_icon.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:share_plus/share_plus.dart';
 
