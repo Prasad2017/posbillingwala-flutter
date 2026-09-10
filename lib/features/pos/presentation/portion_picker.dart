c53a428cdedde10f4346678dc214d982d8e85c79
Prasad <sawantp500@gmail.com>
2026-09-10 19:51:14 +0530

    fix(ui): clean POS print tables encoding and brand imports

--- lib/features/pos/presentation/portion_picker.dart
+++ lib/features/pos/presentation/portion_picker.dart
@@ -1,4 +1,5 @@
-﻿import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:pos_billingwala_v2/core/constants/app_colors.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:pos_billingwala_v2/core/database/app_database.dart';
