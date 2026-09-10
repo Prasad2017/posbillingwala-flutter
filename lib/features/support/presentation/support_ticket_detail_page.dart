71d7b97d5b09425e1988185ea4e25110abb4230c
Prasad <sawantp500@gmail.com>
2026-09-10 19:49:04 +0530

    chore(ui): remove stale presentation imports after redesign audit

--- lib/features/support/presentation/support_ticket_detail_page.dart
+++ lib/features/support/presentation/support_ticket_detail_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pos_billingwala_v2/core/constants/app_colors.dart';
 import 'package:pos_billingwala_v2/core/widgtes/widgtes.dart';
 import 'package:pos_billingwala_v2/core/widgets/app_module_icon.dart';
-import 'package:pos_billingwala_v2/core/widgets/app_module_icon.dart';
 import 'package:pos_billingwala_v2/features/auth/domain/auth_controller.dart';
 import 'package:pos_billingwala_v2/features/support/data/support_api.dart';
 import 'package:pos_billingwala_v2/features/support/data/support_dtos.dart';
