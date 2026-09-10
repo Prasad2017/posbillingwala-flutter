d71476c5c7d342df9108fb129f0f1da64f974c19
Prasad <sawantp500@gmail.com>
2026-09-10 19:51:25 +0530

    fix(ui): clean POS print tables encoding and brand imports

--- lib/features/print/presentation/test_invoice_preview_page.dart
+++ lib/features/print/presentation/test_invoice_preview_page.dart
@@ -1,4 +1,4 @@
-﻿import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pos_billingwala_v2/core/constants/app_colors.dart';
