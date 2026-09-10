ecd2971c2da51891504ecfd332f74e916978d543
Prasad <sawantp500@gmail.com>
2026-09-10 19:55:43 +0530

    fix(ui): complete masters presentation encoding cleanup

--- lib/features/masters/presentation/subcategories_page.dart
+++ lib/features/masters/presentation/subcategories_page.dart
@@ -1,4 +1,4 @@
-﻿import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pos_billingwala_v2/core/constants/app_colors.dart';
 import 'package:pos_billingwala_v2/core/database/app_database.dart';
