945e95bfa4e835014068d010154db97d8f6bd280
Prasad <sawantp500@gmail.com>
2026-09-10 19:47:37 +0530

    fix(ui): remove duplicate imports and hidden encoding markers

--- lib/features/settings/presentation/business_hours_page.dart
+++ lib/features/settings/presentation/business_hours_page.dart
@@ -4,7 +4,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:pos_billingwala_v2/core/widgtes/widgtes.dart';
 import 'package:pos_billingwala_v2/core/constants/app_colors.dart';
 import 'package:pos_billingwala_v2/core/widgets/app_module_icon.dart';
-import 'package:pos_billingwala_v2/core/widgets/app_module_icon.dart';
 import 'package:pos_billingwala_v2/features/auth/domain/auth_controller.dart';
 import 'package:pos_billingwala_v2/features/company/data/company_api.dart';
 import 'package:pos_billingwala_v2/features/company/data/company_dtos.dart';
